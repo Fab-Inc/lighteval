b82979db6a215e6053de63b8876f29d808c19af2
Oliver Garrod <oliver.garrod@fabdata.io>
2026-03-09 14:34:39 +0000

    Switch to using nanotron config imports from nanotron.lighteval_config

--- src/lighteval/config/lighteval_config.py
+++ src/lighteval/config/lighteval_config.py
@@ -24,6 +24,13 @@ from dataclasses import dataclass
 from typing import Dict, Optional, Union
 
 from nanotron.config import Config
+from nanotron.config.lighteval_config import (
+    LightEvalConfig,
+    GenerationArgs,
+    LightEvalLoggingArgs,
+    LightEvalTasksArgs,
+    LightEvalSlurm,
+)
 from nanotron.config.parallelism_config import ParallelismArgs
 from nanotron.generation.sampler import SamplerType
 from nanotron.logging import get_logger
@@ -34,64 +41,64 @@ logger = get_logger(__name__)
 DEFAULT_GENERATION_SEED = 42
 
 
-@dataclass
-class GenerationArgs:
-    sampler: Optional[Union[str, SamplerType]] = None
-    temperature: Optional[float] = None
-    top_k: Optional[int] = None
-    top_p: Optional[float] = None
-    n_samples: Optional[int] = None
-    eos: Optional[str] = None
-    seed: Optional[int] = None
-    use_cache: Optional[bool] = False
+# @dataclass
+# class GenerationArgs:
+#     sampler: Optional[Union[str, SamplerType]] = None
+#     temperature: Optional[float] = None
+#     top_k: Optional[int] = None
+#     top_p: Optional[float] = None
+#     n_samples: Optional[int] = None
+#     eos: Optional[str] = None
+#     seed: Optional[int] = None
+#     use_cache: Optional[bool] = False
 
-    def __post_init__(self):
-        if isinstance(self.sampler, str):
-            self.sampler = SamplerType[self.sampler.upper()]
-        if self.seed is None:
-            self.seed = DEFAULT_GENERATION_SEED
-
-
-@dataclass
-class LightEvalLoggingArgs:
-    """Arguments related to logging for LightEval"""
-
-    output_dir: str
-    save_details: bool = True
-    push_to_hub: bool = False
-    push_to_tensorboard: bool = False
-    public_run: bool = False
-    results_org: str | None = None
-    tensorboard_metric_prefix: str = "eval"
+#     def __post_init__(self):
+#         if isinstance(self.sampler, str):
+#             self.sampler = SamplerType[self.sampler.upper()]
+#         if self.seed is None:
+#             self.seed = DEFAULT_GENERATION_SEED
 
 
-@dataclass
-class LightEvalTasksArgs:
-    """Arguments related to tasks for LightEval"""
+# @dataclass
+# class LightEvalLoggingArgs:
+#     """Arguments related to logging for LightEval"""
 
-    tasks: str
-    custom_tasks: Optional[str] = None
-    max_samples: Optional[int] = None
-    num_fewshot_seeds: Optional[int] = None
+#     output_dir: str
+#     save_details: bool = True
+#     push_to_hub: bool = False
+#     push_to_tensorboard: bool = False
+#     public_run: bool = False
+#     results_org: str | None = None
+#     tensorboard_metric_prefix: str = "eval"
 
-    dataset_loading_processes: int = 8
-    multichoice_continuations_start_space: Optional[bool] = None
-    pairwise_tokenization: bool = False
+
+# @dataclass
+# class LightEvalTasksArgs:
+#     """Arguments related to tasks for LightEval"""
+
+#     tasks: str
+#     custom_tasks: Optional[str] = None
+#     max_samples: Optional[int] = None
+#     num_fewshot_seeds: Optional[int] = None
+
+#     dataset_loading_processes: int = 8
+#     multichoice_continuations_start_space: Optional[bool] = None
+#     pairwise_tokenization: bool = False
 
 
-@dataclass
-class LightEvalConfig:
-    """Arguments related to running LightEval on checkpoints.
+# @dataclass
+# class LightEvalConfig:
+#     """Arguments related to running LightEval on checkpoints.
 
-    All is optional because you can also use this class to later supply arguments to override
-    the saved config when running LightEval after training.
-    """
+#     All is optional because you can also use this class to later supply arguments to override
+#     the saved config when running LightEval after training.
+#     """
 
-    logging: LightEvalLoggingArgs
-    tasks: LightEvalTasksArgs
-    parallelism: ParallelismArgs
-    batch_size: int = 0
-    generation: Optional[Union[GenerationArgs, Dict[str, GenerationArgs]]] = None
+#     logging: LightEvalLoggingArgs
+#     tasks: LightEvalTasksArgs
+#     parallelism: ParallelismArgs
+#     batch_size: int = 0
+#     generation: Optional[Union[GenerationArgs, Dict[str, GenerationArgs]]] = None
 
 
 @dataclass
